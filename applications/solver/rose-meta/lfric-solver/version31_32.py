fecb4b32b429169d8c940e1eecba65528f857f38
Dan Copsey <dan.copsey@metoffice.gov.uk>
2026-04-10 16:55:16 +0100

    Improve command to help black

--- applications/solver/rose-meta/lfric-solver/version31_32.py
+++ applications/solver/rose-meta/lfric-solver/version31_32.py
@@ -41,7 +41,7 @@ class vn31_t400(MacroUpgrade):
 
         # Is this a coupled model? The easiest way to get this is from the number of sea ice categories.
         nice = int(self.get_setting_value(config, ["namelist:jules_sea_seaice", "nice"])
-        is_coupled = nice > 1
+        is_coupled = bool(nice > 1)
  
         # Add the new namelist settings
         self.add_setting(config, ["namelist:jules_sea_seaice", "l_zenith_albedo"], ".false.")
